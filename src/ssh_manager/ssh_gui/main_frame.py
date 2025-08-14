23bfa9845ee46e251ced67a7bc34a2a238b89e61
aaron newman <aaronlnewman@gmail.com>
2025-08-14 17:02:19 +0900

    finished logic for disabling and re-enabling the key size variable based on selected key

--- src/ssh_manager/ssh_gui/main_frame.py
+++ src/ssh_manager/ssh_gui/main_frame.py
@@ -16,10 +16,11 @@ class MainFrame:
         self.frame.grid(column=0, row=0, sticky="nsew")
         self.frame_title = ttk.Label(master=self.frame)
 
-        self.keytype_label = ttk.Label(master=self.frame, text="Keytype")
+        self.keytype_label = ttk.Label(master=self.frame, text="Key-type")
         self.keytype_combobox = ttk.Combobox(master=self.frame, state="readonly")
         self.keytype_combobox.bind("<<ComboboxSelected>>", self.set_keytype_action)
 
+        self.keysize_label = ttk.Label(master=self.frame, text="Key-size")
         self.keysize_combobox = ttk.Combobox(master=self.frame, state="readonly")
 
     def set_frame_title(self, **kwargs):
@@ -42,6 +43,9 @@ class MainFrame:
         self.keytype_label.pack(padx=kwargs.get("padx"), pady=kwargs.get("pady"))
         self.keytype_combobox.pack(padx=kwargs.get("padx"), pady=kwargs.get("pady"))
 
+    def configure_keytype_label(self):
+        pass
+
     def configure_keysize_combobox(self, **kwargs):
         """
         Configure the Key-size Combo-box
@@ -51,13 +55,17 @@ class MainFrame:
         )
         self.keysize_combobox.pack()
 
+    def configure_keyzise_label(self, **kwargs):
+        pass
+
     def set_keytype_action(self, event):
         """
-        add doc-string
+        Disable the Key-size Combo-box if 'ed25519' is selected and clear selection.
+        Re-enable if 'rsa' is selected.
         """
         value = self.keytype_combobox.get()
         if value == "ed25519":
             self.keysize_combobox.config(state=["disabled"])
-            self.keysize_combobox.selection_clear()
-        else:
-            self.keysize_combobox.config(state=["!disabled", "readonly"])
+            self.keysize_combobox.set(value="")
+        elif value == "rsa":
+            self.keysize_combobox.config(state="readonly")
